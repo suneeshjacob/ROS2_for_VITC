e9e925fa2ef7c4b80d745b2b9addcc73ad163d2c
Suneesh Jacob <suneeshjacob@gmail.com>
2023-07-19 17:37:02 +0530

    Update subscriber.m

--- subscriber.m
+++ subscriber.m
@@ -1,7 +1,7 @@
 node = ros2node("message_receiver");
 topic = "/hello_world";
-sub = ros2subscriber(node,topic,@subCallback);
+sub = ros2subscriber(node,topic,@subscriber_callback);
 
-function subCallback(msg)
+function subscriber_callback(msg)
     disp(msg.data);
 end
