1b4c0108b758a08523f636c6b50bb4cf3fcd3433
Suneesh Jacob <suneeshjacob@gmail.com>
2023-07-19 07:58:53 +0530

    Create my_python_subscriber_node.py

---
+++ my_python_subscriber_node.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import time
+
+class MySubscriber(Node):
+    def __init__(self):
+        super().__init__("my_subscriber_node")
+        self.subscriber = self.create_subscription(String, 'my_talker', self.receipt, 5)
+
+    def receipt(self, msg):
+        print(f"[INFO] [{format(time.time(),'0.7f')}] [listener]: I heard: {msg.data}")
+
+def listener():
+    rclpy.init()
+    my_subscriber_object = MySubscriber()
+    try:
+        rclpy.spin(my_subscriber_object)
+    except KeyboardInterrupt:
+        print("Destroying node...")
+        my_subscriber_object.destroy_node()
+    finally:
+        print("Bye bye!")
+
+if __name__ == "__main__":
+    listener()
+
