78cc4fe9c65903f964089fbf5d42b12552ad1848
Suneesh Jacob <suneeshjacob@gmail.com>
2023-07-19 17:35:19 +0530

    Create publisher.m

---
+++ publisher.m
@@ -0,0 +1,12 @@
+node = ros2node("message_publisher");
+topic = "/chatter";
+pub = ros2publisher(node,topic,"std_msgs/String");
+
+msg = ros2message(pub);
+
+
+for i = 1:100
+    msg.data = strcat('Hello World: ',num2str(i));
+    send(pub,msg);
+    pause(1);
+end
