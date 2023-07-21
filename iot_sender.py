a46f0299cf08b4c022e2273c2e88a3d5ed00f836
Suneesh Jacob <suneeshjacob@gmail.com>
2023-07-22 01:20:18 +0530

    Update iot_sender.py

--- iot_sender.py
+++ iot_sender.py
@@ -5,19 +5,25 @@ import requests
 import json
 import time
 
+url = 'https://8863-49-37-148-1.ngrok.io/'
+headers = {'Content-Type':'application/json'}
+data = lambda msg: {'data' : {msg.data}'}
+
 class MySubscriber(Node):
     def __init__(self):
         super().__init__("my_subscriber_node")
         self.subscriber = self.create_subscription(String, 'my_topic', self.receipt, 5)
 
     def receipt(self, msg):
-        print(f"[INFO] [{format(time.time(),'0.7f')}] [listener]: I heard: {msg.data}")
-
-url = 'https://8863-49-37-148-1.ngrok.io/'
-headers = {'Content-Type':'application/json'}
-data = lambda number: {'data' : f'Hello World: {number}'}
-
-for i in range(100):
-	json_data = json.dumps(data(i+1))
+	json_data = json.dumps(data(msg))
 	r = requests.post(url, headers = headers, data = json_data)
 	time.sleep(1)
+        
+
+
+rclpy.init()
+my_subscriber_object = MySubscriber()
+try:
+	rclpy.spin(my_subscriber_object)
+except KeyboardInterrupt:
+	my_subscriber_object.destroy_node()
