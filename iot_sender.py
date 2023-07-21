5446f14b423131becd0bfc276725419828de87fa
Suneesh Jacob <suneeshjacob@gmail.com>
2023-07-22 01:15:35 +0530

    Update iot_sender.py

--- iot_sender.py
+++ iot_sender.py
@@ -1,6 +1,17 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 import requests
 import json
 import time
+
+class MySubscriber(Node):
+    def __init__(self):
+        super().__init__("my_subscriber_node")
+        self.subscriber = self.create_subscription(String, 'my_topic', self.receipt, 5)
+
+    def receipt(self, msg):
+        print(f"[INFO] [{format(time.time(),'0.7f')}] [listener]: I heard: {msg.data}")
 
 url = 'https://8863-49-37-148-1.ngrok.io/'
 headers = {'Content-Type':'application/json'}
